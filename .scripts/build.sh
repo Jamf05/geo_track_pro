caef0205ab31af01d2373ab62ac33b85bbee3a78
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-03 18:00:31 -0500

    feat(chore): actualizados gitignore y scripts analisis de codigo estatico

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Running build script..."
 fvm flutter pub upgrade && \
 fvm flutter pub get && \
 fvm dart run build_runner build --delete-conflicting-outputs && \
--- .scripts/linter.sh
+++ .scripts/linter.sh
@@ -2,7 +2,8 @@
 
 echo "Running linter..."
 
-fvm dart run dart_code_linter:metrics analyze lib --no-fatal-style --no-fatal-warnings --no-fatal-performance
-fvm dart run dart_code_linter:metrics check-unnecessary-nullable lib --fatal-found
-fvm dart run dart_code_linter:metrics check-unused-files lib --fatal-unused
-fvm dart run dart_code_linter:metrics check-unused-code lib --fatal-unused
+fvm dart run dart_code_linter:metrics analyze lib --fatal-style --fatal-warnings --fatal-performance && \
+fvm dart run dart_code_linter:metrics check-unnecessary-nullable lib --fatal-found && \
+fvm dart run dart_code_linter:metrics check-unused-files lib --fatal-unused && \
+fvm dart run dart_code_linter:metrics check-unused-code lib --fatal-unused && \
+fvm dart run dart_code_linter:metrics check-unused-l10n lib --fatal-unused
